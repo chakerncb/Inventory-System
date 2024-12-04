4f1bd6dcfc30ce6c2d1d7e5da07dbca7292c5c60
Chaker Necibi <154261868+chakerncb@users.noreply.github.com>
2024-12-04 17:14:18 +0100

    Add initial server setup, HTML file, and update package dependencies

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+    });
+
+
+    
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
