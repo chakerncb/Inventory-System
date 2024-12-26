a022cb8f668d6a11ea4fa2f85dea2632c9a06b82
Chaker Necibi <154261868+chakerncb@users.noreply.github.com>
2024-12-26 22:52:12 +0100

    .

--- middleware/AdminAuthification.js
+++ middleware/AdminAuthification.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-    // if (req.session && req.session.user) {
-    //     return next();
-    // } else {
-    //     res.redirect('/admin/login');
-    // }
-    next();
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        res.redirect('/admin/login');
+    }
+    // next();
 };
